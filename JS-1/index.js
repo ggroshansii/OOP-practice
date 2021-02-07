ce91a668dfc15cc6ab972ff2e756341976c517c3
ggroshansii <garthgroshansii@gmail.com>
2021-02-07 16:31:40 -0500

    value vs reference types

--- JS-1/index.js
+++ JS-1/index.js
@@ -39,3 +39,22 @@ Circle.apply({}, [1, 2, 3]) //same as call() but can pass array now
 // new String();  <- string constructor; "", '', ``  <- string literals
 // new Boolean();  <- boolean constructor; true, false  <- boolean literals
 // new Number();  <- number constructor; 1, 2, 3 <- number literals
+
+
+//Value VS. Reference Types
+// VALUE type (primitives) = Number, String, Boolean, Symbol, undefined, null
+// REFERENCE types = Object, Function, Array
+
+let x = 10;
+let y = x; // independent of each other; value of x is copied to y
+
+x = 20; // x = 20, but y still equals 10;
+
+let x = {value: 10};
+let y = x; // address/reference of the object's location in memory is stored in x and then this statement copies the address to y, which makes both values point to this object's reference
+
+x.value = 20; // x = 20, and y = 20; 
+
+
+
+//
